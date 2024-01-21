c0c557c9822f58c3e90ce075744f52665b8d46d5
Oleksandr <shapovalov.it@gmail.com>
2024-01-21 19:16:39 +0200

    initial commit

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -8,26 +8,26 @@
 #   value       = module.name.names
 # }
 
-# output "resource_groups" {
-#   description = "All created resource groups"
-#   value       = module.resource_group.resource_groups
-# }
+output "resource_groups" {
+  description = "All created resource groups"
+  value       = module.resource_group.resource_groups
+}
 
-# output "private_dns_zone" {
-#   value = module.private_dns_zone
-# }
+output "private_dns_zone" {
+  value = module.private_dns_zone
+}
 
-# output "private_dns_cname_records" {
-#   value = module.private_dns_cname_record
-# }
+output "private_dns_cname_records" {
+  value = module.private_dns_cname_record
+}
 
-# output "private_dns_ptr_records" {
-#   value = module.private_dns_ptr_record
-# }
+output "private_dns_ptr_records" {
+  value = module.private_dns_ptr_record
+}
 
-# output "private_dns_txt_record" {
-#   value = module.private_dns_txt_record
-# }
+output "private_dns_txt_record" {
+  value = module.private_dns_txt_record
+}
 
 output "private_dns_mx_record" {
   value = module.private_dns_mx_record
