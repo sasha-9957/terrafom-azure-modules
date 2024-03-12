c7d485beee30e971e8ca2689b03a6a87e1b66a34
Oleksandr <shapovalov.it@gmail.com>
2024-03-12 22:39:39 +0200

    update

---
+++ dev/backup/naming-app.tf
@@ -0,0 +1,58 @@
+module "name" {
+  source = "../modules/terraform-azurecaf-name"
+
+  azurecaf_name_params = {
+    main_rg = {
+      name           = var.environment
+      prefixes       = [var.prefixes]
+      suffixes       = [var.location]
+      random_length  = 3
+      resource_type  = "azurerm_resource_group"
+      resource_types = null
+      clean_input    = true
+      random_seed    = null
+      separator      = null
+      passthrough    = null
+      use_slug       = null
+    }
+    main_vnet = {
+      name           = var.environment
+      prefixes       = [var.prefixes]
+      suffixes       = [var.location]
+      random_length  = 3
+      resource_type  = "azurerm_virtual_network"
+      resource_types = null
+      clean_input    = true
+      random_seed    = null
+      separator      = null
+      passthrough    = null
+      use_slug       = null
+    }
+    subnet1 = {
+      name           = var.environment
+      prefixes       = ["${var.prefixes}-1"]
+      suffixes       = [var.location]
+      random_length  = 3
+      resource_type  = "azurerm_subnet"
+      resource_types = null
+      clean_input    = true
+      random_seed    = null
+      separator      = null
+      passthrough    = null
+      use_slug       = null
+    }
+    subnet2 = {
+      name           = var.environment
+      prefixes       = ["${var.prefixes}-2"]
+      suffixes       = [var.location]
+      random_length  = 3
+      resource_type  = "azurerm_subnet"
+      resource_types = null
+      clean_input    = true
+      random_seed    = null
+      separator      = null
+      passthrough    = null
+      use_slug       = null
+    }
+  }
+}
--- dev/main.tf
+++ dev/main.tf
@@ -12,76 +12,96 @@ module "resource_group" {
   }
 }
 
-module "virtual_network" {
-  source = "../modules/terraform-azurerm-virtual-network"
 
-  azurerm_virtual_network_params = {
-    vnet1 = {
-      name                    = module.name.names["main_vnet"].result                     # required
-      resource_group_name     = module.resource_group.resource_groups["main_rg"].name     # required
-      address_space           = var.address_space                                         # required
-      location                = module.resource_group.resource_groups["main_rg"].location # required
-      bgp_community           = null
-      ddos_protection_plan    = []
-      encryption              = []
-      dns_servers             = []
-      edge_zone               = null
-      flow_timeout_in_minutes = null
-      subnet                  = []
-      tags                    = module.tags.tags
+module "azurerm_service_plan" {
+  source = "../modules/terraform-azurerm-service-plan"
+  azurerm_service_plan_params = {
+    main_service_plan = {
+      name                         = "example"                                                 # Required
+      location                     = module.resource_group.resource_groups["main_rg"].location # Required
+      os_type                      = "Linux"                                                   # Required
+      resource_group_name          = module.name.names["main_rg"].result                       # Required
+      sku_name                     = "P1v2"                                                    # Required
+      app_service_environment_id   = null
+      maximum_elastic_worker_count = null
+      worker_count                 = null
+      per_site_scaling_enabled     = null
+      zone_balancing_enabled       = null
+      tags                         = null
     }
   }
 }
 
-module "subnet" {
-  source = "../modules/terraform-azurerm-subnet"
+# module "virtual_network" {
+#   source = "../modules/terraform-azurerm-virtual-network"
 
-  azurerm_subnet_params = {
-    subnet1 = {
-      name                                          = module.name.names["subnet1"].result                   # Required
-      resource_group_name                           = module.resource_group.resource_groups["main_rg"].name # Required
-      virtual_network_name                          = module.virtual_network.virtual_networks["vnet1"].name # Required
-      address_prefixes                              = var.linux_subnet                                      # Required
-      private_endpoint_network_policies_enabled     = null
-      private_link_service_network_policies_enabled = null
-      service_endpoints                             = null
-      service_endpoint_policy_ids                   = null
+#   azurerm_virtual_network_params = {
+#     vnet1 = {
+#       name                    = module.name.names["main_vnet"].result                     # required
+#       resource_group_name     = module.resource_group.resource_groups["main_rg"].name     # required
+#       address_space           = var.address_space                                         # required
+#       location                = module.resource_group.resource_groups["main_rg"].location # required
+#       bgp_community           = null
+#       ddos_protection_plan    = []
+#       encryption              = []
+#       dns_servers             = []
+#       edge_zone               = null
+#       flow_timeout_in_minutes = null
+#       subnet                  = []
+#       tags                    = module.tags.tags
+#     }
+#   }
+# }
 
-      delegation = [
-        {
-          name = "delegation1" # Required
+# module "subnet" {
+#   source = "../modules/terraform-azurerm-subnet"
 
-          service_delegation = [ # Required
-            {
-              name    = "Microsoft.PowerPlatform/vnetaccesslinks"
-              actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-            }
-          ]
-        }
-      ]
-    }
-    subnet2 = {
-      name                                          = module.name.names["subnet2"].result                   # Required
-      resource_group_name                           = module.resource_group.resource_groups["main_rg"].name # Required
-      virtual_network_name                          = module.virtual_network.virtual_networks["vnet1"].name # Required
-      address_prefixes                              = var.powerbi_subnet                                    # Require
-      private_endpoint_network_policies_enabled     = null
-      private_link_service_network_policies_enabled = null
-      service_endpoints                             = null
-      service_endpoint_policy_ids                   = null
+#   azurerm_subnet_params = {
+#     subnet1 = {
+#       name                                          = module.name.names["subnet1"].result                   # Required
+#       resource_group_name                           = module.resource_group.resource_groups["main_rg"].name # Required
+#       virtual_network_name                          = module.virtual_network.virtual_networks["vnet1"].name # Required
+#       address_prefixes                              = var.linux_subnet                                      # Required
+#       private_endpoint_network_policies_enabled     = null
+#       private_link_service_network_policies_enabled = null
+#       service_endpoints                             = null
+#       service_endpoint_policy_ids                   = null
 
-      delegation = [
-        {
-          name = "delegation2" # Required
+#       delegation = [
+#         {
+#           name = "delegation1" # Required
 
-          service_delegation = [ # Required
-            {
-              name    = "Microsoft.PowerPlatform/vnetaccesslinks"
-              actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-            }
-          ]
-        }
-      ]
-    }
-  }
-}
+#           service_delegation = [ # Required
+#             {
+#               name    = "Microsoft.PowerPlatform/vnetaccesslinks"
+#               actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+#             }
+#           ]
+#         }
+#       ]
+#     }
+#     subnet2 = {
+#       name                                          = module.name.names["subnet2"].result                   # Required
+#       resource_group_name                           = module.resource_group.resource_groups["main_rg"].name # Required
+#       virtual_network_name                          = module.virtual_network.virtual_networks["vnet1"].name # Required
+#       address_prefixes                              = var.powerbi_subnet                                    # Require
+#       private_endpoint_network_policies_enabled     = null
+#       private_link_service_network_policies_enabled = null
+#       service_endpoints                             = null
+#       service_endpoint_policy_ids                   = null
+
+#       delegation = [
+#         {
+#           name = "delegation2" # Required
+
+#           service_delegation = [ # Required
+#             {
+#               name    = "Microsoft.PowerPlatform/vnetaccesslinks"
+#               actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+#             }
+#           ]
+#         }
+#       ]
+#     }
+#   }
+# }
